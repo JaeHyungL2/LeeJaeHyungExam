05946a0d67709b888dabe18be1a5775a273cf74e
jh2 <wednestues18@gmail.com>
2024-02-27 20:06:10 +0900

    first

---
+++ src/main/java/org/zerock/leejaehyungexam/LeeJaeHyungExamApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.leejaehyungexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeJaeHyungExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeJaeHyungExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/leejaehyungexam/LeeJaeHyungExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.leejaehyungexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeJaeHyungExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
